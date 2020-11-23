307cc710e3be77cde9bb368e5d41adbdce4fceef
Joel Palmer <joeljpalmer@btinternet.com>
2020-11-23 20:28:36 +0000

    Split js into separate file

---
+++ index.js
@@ -0,0 +1,10 @@
+var LoginButton = document.getElementById("LoginButton")
+    
+function Login(){
+    var Username = prompt("Please enter your username")
+    if (Username != null) {
+        var Password = prompt("Please enter password")
+        }
+    }
+    
+LoginButton.addEventListener("click",Login)
